66fe53ec122757ac3d9c3aece9d9dbc09dd86a46
Derya Tasdogen <derya.tasdogen@gmail.com>
2019-03-12 18:42:05 +0300

    Initial

---
+++ PeopleSearch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PeopleSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            QuestionManager questionManager = new QuestionManager();
+            questionManager.AskQuestion();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PeopleSearch/QuestionManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleSearch
+{
+    public class QuestionManager
+    {
+        private Dictionary<string, string> Answers = null;
+
+        public QuestionManager()
+        {
+            Answers = new Dictionary<string, string>();
+            Answers.Add("Kütüphanede kaç adet kitap vardır?", "28765");
+            Answers.Add("Kütüphanede kaç tür kitap vardır?", "22");
+            Answers.Add("Harry Potterın yazarı kimdir?", "J. K. Rowling");
+            Answers.Add("Harry Potter ne tür bir romandır?", "Fantastik Kurgu");
+        }
+
+        public string GiveAnswer(string question)
+        {
+            if (!Answers.ContainsKey(question))
+                return null;
+
+            return Answers[question];
+        }
+
+        public void AskQuestion()
+        {
+            Console.WriteLine("Sorunuzu Giriniz!");
+            string question = Console.ReadLine();
+            string answer = GiveAnswer(question);
+
+            if (string.IsNullOrEmpty(answer))
+                Console.WriteLine("Soru Bulunamadı!");
+            else
+                Console.WriteLine(answer);
+
+            AskQuestion();
+        }
+    }
+}
