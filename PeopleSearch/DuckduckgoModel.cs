18749d59726d29d17a003ac683a06fa3cca3c765
Derya Tasdogen <derya.tasdogen@gmail.com>
2019-03-12 19:39:10 +0300

    Duckduckgo added

---
+++ PeopleSearch/DuckduckgoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleSearch
+{
+
+    public class DuckduckgoModel
+    {
+        
+        public string AbstractURL { get; set; }
+        public string AbstractSource { get; set; }
+        public string AbstractText { get; set; }
+        public string Abstract { get; set; }
+    }
+}
--- PeopleSearch/QuestionManager.cs
+++ PeopleSearch/QuestionManager.cs
@@ -1,42 +1,54 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Web;
 
 namespace PeopleSearch
 {
     public class QuestionManager
     {
-        private Dictionary<string, string> Answers = null;
-
-        public QuestionManager()
-        {
-            Answers = new Dictionary<string, string>();
-            Answers.Add("Albert Einstein", "Cool guy");
-            Answers.Add("Lacuna Coil", "Cool band");
-            Answers.Add("Within Temptation", "Cooler band");
-            Answers.Add("Harry Potter", "⚡");
-        }
-
         public string GiveAnswer(string question)
         {
             Regex regex = new Regex(@"^(Who is|What is|Where is)\s+");
             string questionRoot = regex.Replace(question, "");
+            string encodedQuestionRoot = HttpUtility.UrlEncode(questionRoot);
+            string url = $"https://api.duckduckgo.com/?q={encodedQuestionRoot}&format=json";
 
-            if (!Answers.ContainsKey(questionRoot))
+            WebClient client = new WebClient();
+            string jsonString = client.DownloadString(url);
+            DuckduckgoModel duckduckgoModel = JsonConvert.DeserializeObject<DuckduckgoModel>(jsonString);
+
+            if (duckduckgoModel == null)
                 return null;
 
-            return Answers[questionRoot];
+            string responseText = duckduckgoModel.Abstract;
+            string abstractSource = duckduckgoModel.AbstractSource;
+            string abstractUrl = duckduckgoModel.AbstractURL;
+
+            if (string.IsNullOrEmpty(responseText))
+                return null;
+
+            if (!string.IsNullOrEmpty(abstractSource))
+                responseText += Environment.NewLine + "From: " + abstractSource;
+
+            if (!string.IsNullOrEmpty(abstractUrl))
+                responseText += Environment.NewLine + "More Detail: " + abstractUrl;
+
+            return responseText;
         }
 
         public void AskQuestion()
         {
-            Console.WriteLine("Sorunuzu Giriniz!");
+            Console.WriteLine("Who are you looking for?");
             string question = Console.ReadLine();
             string answer = GiveAnswer(question);
 
             if (string.IsNullOrEmpty(answer))
-                Console.WriteLine("Soru Bulunamadı!");
+                Console.WriteLine("Sorry. I cannot answer that yet.");
             else
                 Console.WriteLine(answer);
 
